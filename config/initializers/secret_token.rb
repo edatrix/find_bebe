d68ce265a9ae25c98bb55a2d7f346821a93dbfb0
edatrix <lespath@gmail.com>
2013-12-20 10:32:12 -0700

    initial setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FindBebe::Application.config.secret_key_base = '132c7c63be6809131c0933c4fa9155078e040f9dc3bdff78807f68514fe6703cba6b4d4fdd94e71a6a80be8bd116b592895f2704b552cea616ca750c8e626e22'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FindBebe::Application.config.session_store :cookie_store, key: '_find_bebe_session'
