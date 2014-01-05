fcbb5142b8e2d6262991d760e7daefad6304341c
edatrix <lespath@gmail.com>
2014-01-05 15:52:35 -0700

    added seedfile, images to assets, played with formatting of index

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Announcement.delete_all
+Announcement.create(:title => 'Find Bebe',
+  :description =>
+    "Find Bebe is a social pet finder app.",
+  :zip_code => "90210",
+  :image_url =>   '')
+# . . .
+Announcement.create(:title => 'Programming Ruby 1.9',
+  :description =>
+    "Ruby is the fastest growing and most exciting dynamic language
+    out there. If you need to get working programs delivered fast,
+    you should add Ruby to your toolbox.",
+  :zip_code => "90210",
+  :image_url => '')
+# . . .
+
+Announcement.create(:title => 'Rails Test Prescriptions',
+  :description =>
+    "Rails Test Prescriptions is a comprehensive guide to testing
+    Rails applications, covering Test-Driven Development from both a
+    theoretical perspective (why to test) and from a practical perspective
+    (how to test effectively). It covers the core Rails testing tools and
+    procedures for Rails 2 and Rails 3, and introduces popular add-ons,
+    including Cucumber, Shoulda, Machinist, Mocha, and Rcov.",
+  :zip_code => "90210",
+  :image_url => '')
