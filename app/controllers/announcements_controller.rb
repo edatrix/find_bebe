e51f3acbab8c3850ff8444d25f908ce70e210275
edatrix <lespath@gmail.com>
2014-02-16 18:16:36 -0700

    removes comments and cleans up files

--- app/controllers/announcements_controller.rb
+++ app/controllers/announcements_controller.rb
@@ -62,12 +62,11 @@ class AnnouncementsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+
     def set_announcement
       @announcement = Announcement.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def announcement_params
       params.require(:announcement).permit(:title, :description, :zip_code, :image_url)
     end
